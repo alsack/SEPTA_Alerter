ff2684be3ae1f92e8e88bdace078db20197d096f
Andrew Sack <sacko87@gmail.com>
2018-12-16 21:46:50 -0500

    bug fixes

--- SEPTA_Alerter.py
+++ SEPTA_Alerter.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 import requests
 import smtplib
 import sys
@@ -19,32 +20,33 @@ def main():
 		logging.getLogger('logger').info('Running at ' + datetime.datetime.now().ctime())
 		
 		#get the alerts to check for.
-		checks = getTrainsToCheck()
+		trainsToCheck = getTrainsToCheck()
 
-		if len(checks) == 0:
+		if len(trainsToCheck) == 0:
 			logging.getLogger('logger').info("No trains to check")
 			return
 		else:
 			status = 'Looking for trains: '
-			for check in checks:
-				check['found'] = False
-				status += check['train'].rstrip() + ' '
+			for train in trainsToCheck:
+				train['found'] = False
+				status += train['trainNum'].rstrip() + ' '
 			logging.getLogger('logger').info(status)
 
 		#get the status from SEPTA
 		septaStatus = requests.get(septaUrl).json()
 
-		#check its status
+		#check each train from SEPTA and see if anyone wants status for it
 		for train in septaStatus:
-			for check in checks:
-				if train['trainno'] == check['train']:
+			for trainToCheck in trainsToCheck:
+				if train['trainno'] == trainToCheck['trainNum']:
 					#this train is one we're looking to check
-					check['found'] = True
-					updateStatus(train, check)
+					trainToCheck['found'] = True
+					updateStatus(train, trainToCheck)
 		
-		for check in checks:
-			if check['found'] == False:
-				logging.getLogger('logger').warning('Could not find status for train ' + check['train'])
+		for trainToCheck in trainsToCheck:
+			if trainToCheck['found'] == False:
+				logging.getLogger('logger').warning('Could not find status for train ' + trainToCheck['trainNum'])
+				trainNotFound(trainToCheck)
 
 		
 	except:
@@ -72,7 +74,7 @@ def init():
 	return emailUrl, emailPort, emailUser, emailPw, septaUrl	
 
 #returns a list of dictionaries corresponding to trains to check
-#dict keys are 'train', 'email', 'start', 'stop', 'threshold', 'days'
+#dict keys are 'trainNum', 'email', 'start', 'stop', 'threshold', 'days'
 def getTrainsToCheck():
 	f = open('trains.csv', 'r')
 	if (f.mode != 'r'):
@@ -80,7 +82,7 @@ def getTrainsToCheck():
 		return
 	alerts = csv.DictReader(f)
 
-	returnList = [];
+	returnList = []
 	#add the dictionary to the returnList if it is within the day/time window
 	for alert in alerts:
 		if getDayOfWeekString() in alert['days']:
@@ -91,20 +93,78 @@ def getTrainsToCheck():
 			if now >= start and now <= stop:
 				returnList.append(alert)
 			else:
-				#TODO: delete status file if present
-				pass
+				removeStatusFile(alert)
+		else:
+			removeStatusFile(alert)
 	
-	return returnList;
+	return returnList
 	
 def getDayOfWeekString():
 	days = ['M','Tu','W','Th','F','Sa','Su']
 	return days[datetime.datetime.now().weekday()]	
 
-def updateStatus(train, check):
+def removeStatusFile(trainToCheck):
+	#remove status file if it exists
+	statusFileName = getStatusFileName(trainToCheck)
+	if os.path.exists(statusFileName):
+		os.remove(statusFileName)
+
+def updateStatus(train, trainToCheck):
 	late = train['late']
-	print('need to add logic to save status, check if late is past threshold, and send email.')
-	#sendEmail(check['email'], 'test: %d' % late)
+	if isInt(late) != True:
+		#make sure 'late' is an int
+		logging.getLogger('logger').error('Train %s late status is not an Int:' % train['trainno'])
+		logging.getLogger('logger').error(train)
+		return
+	late = int(late)
 	
+	#check for existence of statusFile
+	statusFilename = getStatusFileName(trainToCheck)
+	try :
+		f = open(statusFilename, 'r')
+		#if open() doesn't throw, the file exists.  Keep sending updates until late == 0
+		lastLate = f.readline()
+		f.close()
+		if isInt(lastLate):
+			#compare to current late time
+			if int(lastLate) != late:
+				#send an update email
+				sendTrainLateEmail(trainToCheck, late)
+		else:
+			#delete the status file, log error. send a message to the client
+			os.remove(statusFile)
+			logging.getLogger('logger').error('File ' + statusFIleName + ' had invalid status "' + lastLate + '"')
+			sendTrainLateEmail(trainToCheck, late)
+
+		#close the status file
+		f.close()
+	except:
+		#file does not exist.  Only send if late is greater than threshold
+		threshold = trainToCheck['threshold']
+		if isInt(threshold) and late >= int(threshold):
+			sendTrainLateEmail(trainToCheck, late)
+
+	#if the train is no longer late, delete the status file
+	if late == 0:
+		os.remove(statusFile)
+
+def getStatusFileName(trainToCheck):
+	return trainToCheck['trainNum'] + '.' + trainToCheck['email'] + '.status'
+
+def createStatusFile(filename, late):
+	f = open(filename, 'w')
+	f.write(str(late))
+	f.close()
+
+def sendTrainLateEmail(trainToCheck, late):
+	logging.getLogger('logger').info('Sending status email to ' + trainToCheck['email'] + '. Train ' + trainToCheck['trainNum'] + ' is ' + str(late) + ' minutes late.')
+	sendEmail(trainToCheck['email'], 'Train ' + trainToCheck['trainNum'] + ' is ' + str(late) + ' minutes late.')
+	createStatusFile(getStatusFileName(trainToCheck), late)
+
+def trainNotFound(trainToCheck):
+	#send an email notifying the user that the train doesn't have a status (presumably isn't running).
+	sendEmail(trainToCheck['email'], 'Couldn\'t find Train %s status!' % trainToCheck['trainNum'])
+	logging.getLogger('logger').warning('Sent email to %s notifying about lack of train status' % trainToCheck['email'])
 
 def sendEmail(recipient, subject):
 	message = '''From: %s\nTo: %s\nSubject: %s\n\n''' % (emailUser,recipient,subject)
@@ -117,5 +177,12 @@ def sendEmail(recipient, subject):
 
 class Error(Exception):
 	pass
+
+def isInt(str):
+	try:
+		int(str)
+		return True
+	except:
+		return False
 
 main()
